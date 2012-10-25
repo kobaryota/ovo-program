9c1606bccd0f1ce3bb0cf590e96374a4c1db9287
kobaryota <k2917315@kadai.jp>
2012-10-25 17:28:52 +0900

    furiwake

---
+++ furiwake.pl
@@ -0,0 +1,8 @@
+print "作業場所を入力\n例）/home/ryota/Desktop\n";
+$sagyou = <STDIN>;
+print "作成したいディレクトリの名前を入力\n";
+$dir = <STDIN>;
+system("mkdir $sagyou/$dir");
+print "振り分けたいものの名前を入力\n例）*1007*.txt\n";
+$furi = <STDIN>;
+system("mv $sagyou/$furi $sagyou/$dir/");
