ee517264468e8933b7021b3e48be2dd726dad284
kobaryota <k2917315@kadai.jp>
2012-10-25 16:20:10 +0900

    日にちのdir

---
+++ getindir.pl
@@ -0,0 +1,7 @@
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+$year += 1900;
+$mon += 1;
+print "$year年$mon月$mday日\n";
+system("mkdir /home/ryota/Desktop/ovokoba/time/$year$mon$mday");
+system("mv /home/ryota/Desktop/ovokoba/time/*$year$mon*.pl /home/ryota/Desktop/ovokoba/time/$year$mon$mday");
+
