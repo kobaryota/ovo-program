92bc886936b5aa9e4173d5cae74e46cf799fe8a6
kobaryota <k2917315@kadai.jp>
2012-10-31 14:38:24 +0900

    Update getindir.pl

--- getindir.pl
+++ getindir.pl
@@ -2,6 +2,6 @@
 $year += 1900;
 $mon += 1;
 print "$year年$mon月$mday日\n";
-system("mkdir /home/ryota/Desktop/ovokoba/time/$year$mon$mday");
-system("mv /home/ryota/Desktop/ovokoba/time/*$year$mon*.pl /home/ryota/Desktop/ovokoba/time/$year$mon$mday");
+system("mkdir /Users/koba/Desktop/$year$mon$mday");
+system("mv /Users/koba/Desktop/*$year$mon*.txt /Users/koba/Desktop/$year$mon$mday");
 
