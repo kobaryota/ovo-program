14f7af80eb21f9726e002bad46ecdb024560b671
kobaryota <k2917315@kadai.jp>
2012-11-06 15:34:24 +0900

    Update getindir.pl

--- getindir.pl
+++ getindir.pl
@@ -1,7 +1,27 @@
+#!/usr/bin/perl
+$Ddata = "/home/ryota/Desktop";##"/home/verausr/DATA_vera2txt_RESULT"; ##データが保存されている場所
+$Dido = "opertion";##"verausr@oper-irk:/home/verausr/koba";   ##データを移動させたい場所 
+
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 $year += 1900;
 $mon += 1;
 print "$year年$mon月$mday日\n";
-system("mkdir /Users/koba/Desktop/$year$mon$mday");
-system("mv /Users/koba/Desktop/*$year$mon*.txt /Users/koba/Desktop/$year$mon$mday");
 
+system("ls $Ddata/*txt > $Ddata/tentai.lst");
+open(IN,"$Ddata/tentai.lst");
+@file = <IN>;
+chomp @file;
+close(IN);
+
+system("rm $Ddata/oper-idou.sh");
+
+while () {
+foreach $files (@file){
+  open(OUT,">> $Ddata/oper-idou.sh");
+	print(OUT "scp $files $Dido\n");
+	print(OUT "mv $files $Ddata/$year$mon\n");
+	}
+	close(OUT);
+
+system("sh $Ddata/oper-idou.sh");
+}
